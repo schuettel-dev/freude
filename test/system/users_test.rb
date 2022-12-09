5918b52875a64b7d797af036a7bf8208ea7094a4
mario <github@lxxxvi.ch>
2022-12-09 16:16:53 +0100

    restyle sign up pgae

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -4,10 +4,10 @@ class UsersTest < ApplicationSystemTestCase
   test "sign up" do
     visit root_path
 
-    assert_selector "h1", text: "Join"
+    assert_selector "h1", text: "Join the games"
 
     fill_in "Name", with: "Toad"
-    click_on "Join games"
+    click_on "Sign up"
 
     assert_selector "h1", text: "My games"
   end
@@ -16,7 +16,7 @@ class UsersTest < ApplicationSystemTestCase
     visit root_path
 
     fill_in "Name", with: "Yoshi"
-    click_on "Join games"
+    click_on "Sign up"
     click_on "Sign out"
     click_on "Rejoin as Yoshi"
 
