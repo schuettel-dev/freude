de29c7b816a8e4f69586ba7f31118f7352e94222
mario <github@lxxxvi.ch>
2023-02-15 16:29:46 +0100

    move controller test

---
+++ test/controllers/games/beatle/playlists_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Games::Beatle::PlaylistsControllerTest < ActionDispatch::IntegrationTest
+  test "GET show" do
+    assert false
+  end
+end
--- test/controllers/games/beatles/playlists_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Games::Beatles::PlaylistsControllerTest < ActionDispatch::IntegrationTest
-  test "GET show" do
-    assert false
-  end
-end
