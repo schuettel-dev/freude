f777bccbf88406d79e4c4dd7cee06d259a4b0259
mario <github@lxxxvi.ch>
2023-02-15 16:29:11 +0100

    add test for Games::Beatle::PlaylistComponentTest

--- test/components/games/beatle/playlist_component_test.rb
+++ test/components/games/beatle/playlist_component_test.rb
@@ -1,7 +1,46 @@
 require "test_helper"
 
 class Games::Beatle::PlaylistComponentTest < ViewComponent::TestCase
-  test "render" do
-    assert false
+  test "render, phase collecting" do
+    playlist = player_beatle_playlists(:mario_player_in_beatle_mario_bros_playlist)
+    user = users(:mario)
+
+    render_inline new_component(playlist:, user:)
+
+    assert_selector ".games--beatle--playlist--song-url-status", text: "URL is valid", count: 1
+    assert_selector ".games--beatle--playlist--song-url-status", text: "URL is invalid", count: 1
+    assert_selector ".games--beatle--playlist--song-url-status", text: "URL is blank", count: 1
+
+    assert_selector "iframe", count: 1
+    assert_field "Song 1", with: "https://open.spotify.com/track/6lDyt5CQQAWnHhJyGczaBM?si=6a49db56dadc4a7c"
+    assert_field "Song 2", with: "https://todo"
+    assert_field "Song 3" do |field|
+      assert_predicate field.value, :nil?
+    end
+
+    assert_button "Save playlist"
+    assert_link "Hide playlist"
+  end
+
+  test "render, phase guessing" do
+    games(:beatle_mario_bros).guessing!
+
+    playlist = player_beatle_playlists(:mario_player_in_beatle_mario_bros_playlist)
+    user = users(:mario)
+
+    render_inline new_component(playlist:, user:)
+
+    assert_no_selector "input"
+  end
+
+  test "render, phase ended" do
+    games(:beatle_mario_bros).ended!
+
+    playlist = player_beatle_playlists(:mario_player_in_beatle_mario_bros_playlist)
+    user = users(:mario)
+
+    render_inline new_component(playlist:, user:)
+
+    assert_no_selector "input"
   end
 end
