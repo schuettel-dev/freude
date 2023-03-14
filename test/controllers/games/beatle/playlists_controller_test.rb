f5b5785494cb2ed350df28a6ce2fb8f51d12fa2a
mario <github@lxxxvi.ch>
2023-03-14 16:40:02 +0100

    add Games::Beatle::PlaylistsControllerTest

--- test/controllers/games/beatle/playlists_controller_test.rb
+++ test/controllers/games/beatle/playlists_controller_test.rb
@@ -2,6 +2,27 @@ require "test_helper"
 
 class Games::Beatle::PlaylistsControllerTest < ActionDispatch::IntegrationTest
   test "GET show" do
-    assert false
+    sign_in(:mario)
+    game = games(:beatle_mario_bros)
+    get game_beatle_playlist_path(game)
+    assert_response :success
+  end
+
+  test "PUT update" do
+    sign_in(:mario)
+    game = games(:beatle_mario_bros)
+    playlist = player_beatle_playlists(:mario_player_in_beatle_mario_bros_playlist)
+
+    assert_changes -> { playlist.song_2_url }, to: "https://something.else" do
+      put game_beatle_playlist_path(game), params: {
+        player_beatle_playlist: {
+          song_2_url: "https://something.else"
+        }
+      }
+      playlist.reload
+    end
+
+    follow_redirect!
+    assert_response :success
   end
 end
