f0aad2ba2c4dc7e8a4b6626b88f0ee017f862bac
mario <github@lxxxvi.ch>
2023-03-14 17:18:36 +0100

    improve/cleanup EmbeddedPlayerComponentTest

--- test/components/embedded_player_component_test.rb
+++ test/components/embedded_player_component_test.rb
@@ -1,9 +1,12 @@
 require "test_helper"
 
 class EmbeddedPlayerComponentTest < ViewComponent::TestCase
+  setup do
+    @playlist = player_beatle_playlists(:mario_player_in_beatle_mario_bros_playlist)
+  end
+
   test "render, valid spotify" do
-    playlist = player_beatle_playlists(:mario_player_in_beatle_mario_bros_playlist)
-    render_inline new_component(song_url: playlist.song_urls.first)
+    render_inline new_component(song_url: @playlist.song_urls.first)
 
     assert_selector "iframe" do |element|
       assert_match %r{^https://open.spotify.com/embed/.*$}, element[:src]
@@ -11,15 +14,13 @@ class EmbeddedPlayerComponentTest < ViewComponent::TestCase
   end
 
   test "render, invalid" do
-    playlist = player_beatle_playlists(:mario_player_in_beatle_mario_bros_playlist)
-    render_inline new_component(song_url: playlist.song_urls.second)
+    render_inline new_component(song_url: @playlist.song_urls.second)
 
     assert_no_selector "iframe"
   end
 
   test "render, blank" do
-    playlist = player_beatle_playlists(:mario_player_in_beatle_mario_bros_playlist)
-    render_inline new_component(song_url: playlist.song_urls.third)
+    render_inline new_component(song_url: @playlist.song_urls.third)
 
     assert_no_selector "iframe"
   end
