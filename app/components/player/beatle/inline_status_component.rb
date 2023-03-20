04884f43721ef839d08ae1bc8e037f35ecbefb19
mario <github@lxxxvi.ch>
2023-03-20 16:51:34 +0100

    improve UX for current phase

--- app/components/player/beatle/inline_status_component.rb
+++ app/components/player/beatle/inline_status_component.rb
@@ -1,8 +1,13 @@
 class Player::Beatle::InlineStatusComponent < ApplicationComponent
-  attr_reader :player
+  attr_reader :player, :args
 
-  def initialize(player:)
+  DEFAULT_OPTIONS = {
+    class: "h-6 w-6"
+  }.freeze
+
+  def initialize(player:, **args)
     @player = player
+    @args = DEFAULT_OPTIONS.deep_dup.merge(args)
     super()
   end
 
