c97f8ef86863fecd41bb29535e632e192f5b3e08
mario <github@lxxxvi.ch>
2023-03-28 13:44:36 +0200

    add Games::Beatle::PlaylistGuessTest

--- test/models/games/beatle/playlist_guess_test.rb
+++ test/models/games/beatle/playlist_guess_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class Games::Beatle::PlaylistGuessTest < ActiveSupport::TestCase
-  test "todo" do
-    skip "TODO"
+  test "validates" do
+    playlist_guess = games_beatle_playlist_guesses(:jerry_player_in_beatle_seinfeld_guessing_elaine).dup
+    playlist_guess.points = 2
+
+    assert_not_predicate playlist_guess, :valid?
+
+    playlist_guess.errors.to_a.tap do |errors|
+      assert_includes errors, "Points is not included in the list"
+      assert_includes errors, "Guessing player has already been taken"
+    end
   end
 end
