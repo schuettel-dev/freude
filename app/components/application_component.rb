a32a12b0644f39a7319502abcabd85b2763e642a
mario <github@lxxxvi.ch>
2023-03-29 11:51:43 +0200

    wip, TODO: Guessing section

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -2,4 +2,8 @@ class ApplicationComponent < ViewComponent::Base
   include Turbo::FramesHelper
   include Turbo::StreamsHelper
   include ActionView::RecordIdentifier
+
+  def to_css_class
+    self.class.to_s.underscore.gsub(%r{/}, "__").dasherize
+  end
 end
--- test/components/games/beatle/current_phase_section_component_test.rb
+++ test/components/games/beatle/current_phase_section_component_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class Games::Beatle::CurrentPhaseSectionComponentTest < ViewComponent::TestCase
-  test "render" do
-    skip "to be implemented"
+  test "render, collecting phase" do
+    games(:beatle_seinfeld).collecting!
+    player = players(:jerry_player_in_beatle_seinfeld)
+    render_inline new_component(player:)
+
+    assert_show_playlist_link
+    assert_selector "section", count: 1
+    assert_selector ".games--beatle--playlist--inline-status-component", count: 1
+  end
+
+  test "render, guessing phase" do
+    games(:beatle_seinfeld).guessing!
+    player = players(:jerry_player_in_beatle_seinfeld)
+    render_inline new_component(player:)
+
+    assert_show_playlist_link
+    assert_selector "section", count: 2
+    assert_selector ".games--beatle--playlist--inline-status-component", count: 0
+  end
+
+  test "render, ended phase" do
+    games(:beatle_seinfeld).guessing!
+    player = players(:jerry_player_in_beatle_seinfeld)
+    render_inline new_component(player:)
+
+    assert_selector "section", count: 2
+    assert_selector ".games--beatle--playlist--inline-status-component", count: 0
+    assert_show_playlist_link
+  end
+
+  private
+
+  def assert_show_playlist_link
+    assert_link "Show playlist", href: "/games/BEATLEJERRYURLIDENTIFIER/beatle/playlist"
   end
 end
