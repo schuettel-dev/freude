f84a41b35bf9b72ec4f8a567e7790a3230b63071
mario <github@lxxxvi.ch>
2023-03-31 08:09:32 +0200

    change colors for Players::FinalRankComponent

--- app/components/players/final_rank_component.rb
+++ app/components/players/final_rank_component.rb
@@ -1,11 +1,11 @@
 class Players::FinalRankComponent < ApplicationComponent
   RANK_COLORS = {
-    1 => "from-yellow-300 to-yellow-500",
-    2 => "from-zinc-200 to-zinc-400",
-    3 => "from-amber-600 to-amber-800 text-white"
+    1 => "bg-yellow-400",
+    2 => "bg-zinc-300",
+    3 => "bg-amber-700 text-white"
   }.freeze
 
-  DEFAULT_BG_COLOR = "from-white to-gray-200".freeze
+  DEFAULT_BG_COLOR = "bg-gray-200".freeze
 
   def initialize(player:)
     @player = player
@@ -22,7 +22,7 @@ class Players::FinalRankComponent < ApplicationComponent
 
   def css_classes
     class_names(
-      "inline-flex w-8 h-8 rounded-full text-xs items-center justify-center bg-gradient-to-br",
+      "inline-flex w-8 h-8 rounded-full text-xs items-center justify-center",
       bg_color_classes
     )
   end
