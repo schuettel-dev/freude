25eb4af5cb556684f9f28b3a6d68dbcb89edc15f
mario <github@lxxxvi.ch>
2023-04-06 15:49:38 +0200

    Games::Beatle::PlaylistGuess::FormComponentTest

--- test/components/games/beatle/playlist_guess/form_component_test.rb
+++ test/components/games/beatle/playlist_guess/form_component_test.rb
@@ -2,6 +2,18 @@ require "test_helper"
 
 class Games::Beatle::PlaylistGuess::FormComponentTest < ViewComponent::TestCase
   test "render" do
-    assert false
+    playlist_guess = games_beatle_playlist_guesses(:jerry_player_in_beatle_seinfeld_guessing_elaine)
+    render_inline new_component(playlist_guess:)
+
+    assert_selector "iframe", count: 3
+    assert_selector "select" do |element|
+      assert_equal(
+        ["Pick your guess...", "George (Selected 1 time)", "Kramer (Selected 1 time)", "Laney (Selected 1 time)"],
+        element.find_css("option").map(&:text)
+      )
+    end
+    assert_selector "input[type='submit']" do |element|
+      assert_equal "Save guess", element["value"]
+    end
   end
 end
