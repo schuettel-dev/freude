18e80de16c23eb8502bcaf2ee0ef8cbb86e63a40
mario <github@lxxxvi.ch>
2023-04-08 16:14:09 +0200

    add Games::Beatle::Playlist::DotComponentTest

--- test/components/games/beatle/playlist/dot_component_test.rb
+++ test/components/games/beatle/playlist/dot_component_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class Games::Beatle::Playlist::DotComponentTest < ViewComponent::TestCase
-  test "render" do
-    assert false
+  delegate :url_helpers, to: "Rails.application.routes"
+
+  test "render, current playlist" do
+    playlist = games_beatle_playlists(:jerry_player_in_beatle_seinfeld_playlist)
+    render_inline(new_component(playlist:, current_playlist: playlist)) { "X" }
+
+    assert_selector "span", text: "X" do |element|
+      assert_equal "Jerry", element["title"]
+    end
+  end
+
+  test "render, not current playlist" do
+    current_playlist = games_beatle_playlists(:elaine_player_in_beatle_seinfeld_playlist)
+    playlist = games_beatle_playlists(:jerry_player_in_beatle_seinfeld_playlist)
+    render_inline(new_component(playlist:, current_playlist:)) { "X" }
+
+    assert_link "X", href: url_helpers.game_beatle_playlist_path(playlist.game, playlist) do |element|
+      assert_equal "Jerry", element["title"]
+    end
   end
 end
