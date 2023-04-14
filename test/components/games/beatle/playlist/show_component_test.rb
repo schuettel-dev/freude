284837b36e8e8f5ba2f1690b79067fddafe3b114
mario <github@lxxxvi.ch>
2023-04-14 14:44:41 +0200

    fix tests

--- test/components/games/beatle/playlist/show_component_test.rb
+++ test/components/games/beatle/playlist/show_component_test.rb
@@ -5,7 +5,7 @@ class Games::Beatle::Playlist::ShowComponentTest < ViewComponent::TestCase
     playlist = games_beatle_playlists(:jerry_player_in_beatle_seinfeld_playlist)
     player = players(:jerry_player_in_beatle_seinfeld)
 
-    render_inline new_component(playlist:, player:)
+    render_inline new_component(playlist:, current_player: player)
 
     assert_selector "iframe", count: 3
   end
@@ -17,25 +17,10 @@ class Games::Beatle::Playlist::ShowComponentTest < ViewComponent::TestCase
     other_player = players(:george_player_in_beatle_seinfeld)
     playlist.guesses.update_all(guessed_player_id: other_player.id)
 
-    render_inline new_component(playlist:, player:)
+    render_inline new_component(playlist:, current_player: player)
 
     assert_text "No one got that right!"
-    assert_text "They thought it belonged to"
-    assert_match(/George[\n ]*3x/, page.native.text)
-  end
-
-  test "render, own playlist, one guessed it right" do
-    playlist = games_beatle_playlists(:jerry_player_in_beatle_seinfeld_playlist)
-    player = players(:jerry_player_in_beatle_seinfeld)
-    other_player = players(:george_player_in_beatle_seinfeld)
-    playlist.guesses.update_all(guessed_player_id: other_player.id)
-    playlist.guesses.first.update!(guessed_player: playlist.player)
-
-    render_inline new_component(playlist:, player:)
-
-    assert_text "Only someone got that right!"
-    assert_text "The rest thought it belonged to"
-    assert_match(/George[\n ]*2x/, page.native.text)
+    assert_text "It was believed it belonged to"
   end
 
   test "render, own playlist, multiple guessed it right" do
@@ -45,11 +30,10 @@ class Games::Beatle::Playlist::ShowComponentTest < ViewComponent::TestCase
     playlist.guesses.update_all(guessed_player_id: player.id)
     playlist.guesses.first.update!(guessed_player: other_player)
 
-    render_inline new_component(playlist:, player:)
+    render_inline new_component(playlist:, current_player: player)
 
     assert_text "2 players got that right!"
-    assert_text "The rest thought it belonged to"
-    assert_match(/George[\n ]*1x/, page.native.text)
+    assert_text "It was believed it belonged to"
   end
 
   test "render, own playlist, all guessed it right" do
@@ -57,10 +41,10 @@ class Games::Beatle::Playlist::ShowComponentTest < ViewComponent::TestCase
     player = players(:jerry_player_in_beatle_seinfeld)
     playlist.guesses.update_all(guessed_player_id: player.id)
 
-    render_inline new_component(playlist:, player:)
+    render_inline new_component(playlist:, current_player: player)
 
     assert_text "Everyone got that right!"
-    assert_no_text "The rest thought it belonged to"
+    assert_no_text "It was believed it belonged to"
   end
 
   # other playlist
@@ -70,69 +54,24 @@ class Games::Beatle::Playlist::ShowComponentTest < ViewComponent::TestCase
     other_player = players(:george_player_in_beatle_seinfeld)
     playlist.guesses.update_all(guessed_player_id: other_player.id)
 
-    render_inline new_component(playlist:, player:)
+    render_inline new_component(playlist:, current_player: player)
 
     assert_text "No one got that right!"
-    assert_text "The rest thought it belonged to"
-    assert_match(/George[\n ]*3x/, page.native.text)
+    assert_text "It was believed it belonged to"
   end
 
-  test "render, other playlist, one guessed it right, not current player" do
-    playlist = games_beatle_playlists(:elaine_player_in_beatle_seinfeld_playlist)
-    player = players(:jerry_player_in_beatle_seinfeld)
-    other_player = players(:george_player_in_beatle_seinfeld)
-    playlist.guesses.update_all(guessed_player_id: other_player.id)
-    playlist.guesses.where.not(player:).first.update!(guessed_player: playlist.player)
-
-    render_inline new_component(playlist:, player:)
-
-    assert_text "Only someone got that right, but not you!"
-    assert_text "You thought it was George."
-    assert_text "The rest thought it belonged to"
-    assert_match(/George[\n ]*2x/, page.native.text)
-  end
-
-  test "render, other playlist, one guessed it right, current player" do
-    playlist = games_beatle_playlists(:elaine_player_in_beatle_seinfeld_playlist)
-    player = players(:jerry_player_in_beatle_seinfeld)
-    other_player = players(:george_player_in_beatle_seinfeld)
-    playlist.guesses.update_all(guessed_player_id: other_player.id)
-    playlist.guesses.find_by(player:).update!(guessed_player: playlist.player)
-
-    render_inline new_component(playlist:, player:)
-
-    assert_text "Only you got that right!"
-    assert_text "The rest thought it belonged to"
-    assert_match(/George[\n ]*2x/, page.native.text)
-  end
-
-  test "render, other playlist, multiple guessed it right, excluding current player" do
+  test "render, other playlist, multiple guessed it right" do
     playlist = games_beatle_playlists(:elaine_player_in_beatle_seinfeld_playlist)
     player = players(:jerry_player_in_beatle_seinfeld)
     other_player = players(:george_player_in_beatle_seinfeld)
     playlist.guesses.update_all(guessed_player_id: playlist.player.id)
     playlist.guesses.find_by(player:).update!(guessed_player: other_player)
 
-    render_inline new_component(playlist:, player:)
+    render_inline new_component(playlist:, current_player: player)
 
-    assert_text "2 players got that right, but not you!"
+    assert_text "2 players got that right!"
     assert_text "You thought it was George."
-    assert_text "The rest thought it belonged to"
-    assert_match(/George[\n ]*1x/, page.native.text)
-  end
-
-  test "render, other playlist, multiple guessed it right, including current player" do
-    playlist = games_beatle_playlists(:elaine_player_in_beatle_seinfeld_playlist)
-    player = players(:jerry_player_in_beatle_seinfeld)
-    other_player = players(:george_player_in_beatle_seinfeld)
-    playlist.guesses.update_all(guessed_player_id: playlist.player.id)
-    playlist.guesses.where.not(player:).first.update!(guessed_player: other_player)
-
-    render_inline new_component(playlist:, player:)
-
-    assert_text "2 players got that right, including you!"
-    assert_text "The rest thought it belonged to"
-    assert_match(/George[\n ]*1x/, page.native.text)
+    assert_text "It was believed it belonged to"
   end
 
   test "render, other playlist, all guessed it right" do
@@ -140,7 +79,7 @@ class Games::Beatle::Playlist::ShowComponentTest < ViewComponent::TestCase
     player = players(:jerry_player_in_beatle_seinfeld)
     playlist.guesses.update_all(guessed_player_id: playlist.player.id)
 
-    render_inline new_component(playlist:, player:)
+    render_inline new_component(playlist:, current_player: player)
 
     assert_text "Everyone got that right!"
   end
